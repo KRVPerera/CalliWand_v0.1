8ba3856fe89b42011ce47092d6507a566418e832
KRVPerera <rukshan.viduranga@gmail.com>
2015-08-04 22:10:05 +0530

    Arduino codes

---
+++ Arduino/serialTest/serialTest.ino
@@ -0,0 +1,19 @@
+const int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  Serial.begin(38400);
+  Serial.print('a');
+  char a = 'b';
+  while (a != 'a'){
+    a = Serial.read();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, LOW);
+  delay(5000);
+}
